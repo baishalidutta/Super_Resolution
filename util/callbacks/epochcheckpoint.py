e08e61a55da4f89b2b5d3f2518dce6397ec7fc8a
Baishali Dutta <me@itsbaishali.com>
2022-08-29 20:39:54 +0200

    Updated variable naming case in epoch checkpoint

--- util/callbacks/epochcheckpoint.py
+++ util/callbacks/epochcheckpoint.py
@@ -8,22 +8,22 @@ from tensorflow.keras.callbacks import Callback
 import os
 
 class EpochCheckpoint(Callback):
-	def __init__(self, outputPath, every=5, startAt=0):
+	def __init__(self, output_path, every=5, start_at=0):
 		# call the parent constructor
 		super(Callback, self).__init__()
 
 		# store the base output path for the model, the number of
 		# epochs that must pass before the model is serialized to
 		# disk and the current epoch value
-		self.outputPath = outputPath
+		self.output_path = output_path
 		self.every = every
-		self.intEpoch = startAt
+		self.int_epoch = start_at
 
 	def on_epoch_end(self, epoch, logs={}):
 		# check to see if the model should be serialized to disk
 		if (self.intEpoch + 1) % self.every == 0:
-			p = os.path.sep.join([self.outputPath,
-				"epoch_{}.hdf5".format(self.intEpoch + 1)])
+			p = os.path.sep.join([self.output_path,
+				"epoch_{}.hdf5".format(self.int_epoch + 1)])
 			self.model.save(p, overwrite=True)
 
 		# increment the internal epoch counter
