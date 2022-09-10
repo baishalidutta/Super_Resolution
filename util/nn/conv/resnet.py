fba1eb73ca9870aef46c1c05bd4157f74c81ac2c
Baishali Dutta <me@itsbaishali.com>
2022-09-10 20:58:53 +0200

    Updated variable cases in resnet

--- util/nn/conv/resnet.py
+++ util/nn/conv/resnet.py
@@ -12,22 +12,22 @@ from tensorflow.keras.regularizers import l2
 
 class ResNet:
     @staticmethod
-    def residual_module(data, K, stride, chanDim, red=False,
-                        reg=0.0001, bnEps=2e-5, bnMom=0.9):
+    def residual_module(data, K, stride, chan_dim, red=False,
+                        reg=0.0001, bn_eps=2e-5, bn_mom=0.9):
         # the shortcut branch of the ResNet module should be
         # initialize as the input (identity) data
         shortcut = data
 
         # the first block of the ResNet module are the 1x1 CONVs
-        bn1 = BatchNormalization(axis=chanDim, epsilon=bnEps,
-                                 momentum=bnMom)(data)
+        bn1 = BatchNormalization(axis=chan_dim, epsilon=bn_eps,
+                                 momentum=bn_mom)(data)
         act1 = Activation("relu")(bn1)
         conv1 = Conv2D(int(K * 0.25), (1, 1), use_bias=False,
                        kernel_regularizer=l2(reg))(act1)
 
         # the second block of the ResNet module are the 3x3 CONVs
-        bn2 = BatchNormalization(axis=chanDim, epsilon=bnEps,
-                                 momentum=bnMom)(conv1)
+        bn2 = BatchNormalization(axis=chan_dim, epsilon=bn_eps,
+                                 momentum=bn_mom)(conv1)
         act2 = Activation("relu")(bn2)
         conv2 = Conv2D(int(K * 0.25), (3, 3), strides=stride,
                        padding="same", use_bias=False,
@@ -35,8 +35,8 @@ class ResNet:
 
         # the third block of the ResNet module is another set of 1x1
         # CONVs
-        bn3 = BatchNormalization(axis=chanDim, epsilon=bnEps,
-                                 momentum=bnMom)(conv2)
+        bn3 = BatchNormalization(axis=chan_dim, epsilon=bn_eps,
+                                 momentum=bn_mom)(conv2)
         act3 = Activation("relu")(bn3)
         conv3 = Conv2D(K, (1, 1), use_bias=False,
                        kernel_regularizer=l2(reg))(act3)
@@ -55,7 +55,7 @@ class ResNet:
 
     @staticmethod
     def build(width, height, depth, classes, stages, filters,
-              reg=0.0001, bnEps=2e-5, bnMom=0.9, dataset="cifar"):
+              reg=0.0001, bn_eps=2e-5, bn_mom=0.9, dataset="cifar"):
         # initialize the input shape to be "channels last" and the
         # channels dimension itself
         inputShape = (height, width, depth)
@@ -69,8 +69,8 @@ class ResNet:
 
         # set the input and apply BN
         inputs = Input(shape=inputShape)
-        x = BatchNormalization(axis=chanDim, epsilon=bnEps,
-                               momentum=bnMom)(inputs)
+        x = BatchNormalization(axis=chanDim, epsilon=bn_eps,
+                               momentum=bn_mom)(inputs)
 
         # check if we are utilizing the CIFAR dataset
         if dataset == "cifar":
@@ -83,8 +83,8 @@ class ResNet:
             # apply CONV => BN => ACT => POOL to reduce spatial size
             x = Conv2D(filters[0], (5, 5), use_bias=False,
                        padding="same", kernel_regularizer=l2(reg))(x)
-            x = BatchNormalization(axis=chanDim, epsilon=bnEps,
-                                   momentum=bnMom)(x)
+            x = BatchNormalization(axis=chanDim, epsilon=bn_eps,
+                                   momentum=bn_mom)(x)
             x = Activation("relu")(x)
             x = ZeroPadding2D((1, 1))(x)
             x = MaxPooling2D((3, 3), strides=(2, 2))(x)
@@ -95,17 +95,17 @@ class ResNet:
             # used to reduce the spatial size of the input volume
             stride = (1, 1) if i == 0 else (2, 2)
             x = ResNet.residual_module(x, filters[i + 1], stride,
-                                       chanDim, red=True, bnEps=bnEps, bnMom=bnMom)
+                                       chanDim, red=True, bnEps=bn_eps, bnMom=bn_mom)
 
             # loop over the number of layers in the stage
             for j in range(0, stages[i] - 1):
                 # apply a ResNet module
                 x = ResNet.residual_module(x, filters[i + 1],
-                                           (1, 1), chanDim, bnEps=bnEps, bnMom=bnMom)
+                                           (1, 1), chanDim, bnEps=bn_eps, bnMom=bn_mom)
 
         # apply BN => ACT => POOL
-        x = BatchNormalization(axis=chanDim, epsilon=bnEps,
-                               momentum=bnMom)(x)
+        x = BatchNormalization(axis=chanDim, epsilon=bn_eps,
+                               momentum=bn_mom)(x)
         x = Activation("relu")(x)
         x = AveragePooling2D((8, 8))(x)
 
