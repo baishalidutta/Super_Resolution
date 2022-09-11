bc158d5de77c6c731c23109575da0f9b78c861d2
Baishali Dutta <me@itsbaishali.com>
2022-09-11 20:59:39 +0200

    Formatted and Updated variable cases in srcnn

--- util/nn/conv/srcnn.py
+++ util/nn/conv/srcnn.py
@@ -3,33 +3,33 @@ __copyright__ = "Copyright (C) 2021-2022 Baishali Dutta"
 __license__ = "Apache License 2.0"
 __version__ = "0.1"
 
+from tensorflow.keras import backend as K
+from tensorflow.keras.layers import Conv2D, Activation
 # import the necessary packages
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Conv2D
-from tensorflow.keras.layers import Activation
-from tensorflow.keras import backend as K
+
 
 class SRCNN:
-	@staticmethod
-	def build(width, height, depth):
-		# initialize the model
-		model = Sequential()
-		inputShape = (height, width, depth)
+    @staticmethod
+    def build(width, height, depth):
+        # initialize the model
+        model = Sequential()
+        inputShape = (height, width, depth)
 
-		# if we are using "channels first", update the input shape
-		if K.image_data_format() == "channels_first":
-			inputShape = (depth, height, width)
+        # if we are using "channels first", update the input shape
+        if K.image_data_format() == "channels_first":
+            input_shape = (depth, height, width)
 
-		# the entire SRCNN architecture consists of three CONV =>
-		# RELU layers with *no* zero-padding
-		model.add(Conv2D(64, (9, 9), kernel_initializer="he_normal",
-			input_shape=inputShape))
-		model.add(Activation("relu"))
-		model.add(Conv2D(32, (1, 1), kernel_initializer="he_normal"))
-		model.add(Activation("relu"))
-		model.add(Conv2D(depth, (5, 5),
-			kernel_initializer="he_normal"))
-		model.add(Activation("relu"))
+        # the entire SRCNN architecture consists of three CONV =>
+        # RELU layers with *no* zero-padding
+        model.add(Conv2D(64, (9, 9), kernel_initializer="he_normal",
+                         input_shape=input_shape))
+        model.add(Activation("relu"))
+        model.add(Conv2D(32, (1, 1), kernel_initializer="he_normal"))
+        model.add(Activation("relu"))
+        model.add(Conv2D(depth, (5, 5),
+                         kernel_initializer="he_normal"))
+        model.add(Activation("relu"))
 
-		# return the constructed network architecture
-		return model
+        # return the constructed network architecture
+        return model
